c5ed2ed71d8f62fd0183d828577a4b1373188ac9
Bach Viet Hieu <hieubv@volio.vn>
2023-11-20 23:12:08 +0700

    wip

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: print <message>")
+		return
+	}
+
+	message := strings.Join(os.Args[1:], " ")
+	a := strings.Split(message, " ")
+	if a[len(a)-1] == "help" {
+		fmt.Println("bạn muốn giúp gì? Vui lòng nhấn sử dụng: example help")
+		return
+	}
+	for _, i := range a {
+		fmt.Println(string(i))
+
+	}
+}
